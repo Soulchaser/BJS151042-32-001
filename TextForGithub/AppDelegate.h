bed7a463250d196efc4bdd88f780e87bc0de2806
hanxiaolong <hanxiaolong1116@163.com>
2016-01-14 10:13:06 +0800

    第一次提交，版本1.0

---
+++ TextForGithub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TextForGithub
+//
+//  Created by hanxiaolong on 16/1/14.
+//  Copyright © 2016年 hanxiaolong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TextForGithub/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TextForGithub
+//
+//  Created by hanxiaolong on 16/1/14.
+//  Copyright © 2016年 hanxiaolong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
